c50448842a859bc6b1ccd0f9961a2f5d29ef1a38
Francois Baud <fxbaud@gmail.com>
2020-02-27 11:07:01 +0000

    removed dependencies from the slcak bot

--- my-bot/config/environment.rb
+++
@@ -1,4 +0,0 @@
-require 'bundler/setup'
-Bundler.require
-require_relative '../lib/bot'
-SlackRubyBot::Client.logger.level = Logger::WARN
--- my-bot/lib/bot.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../config/environment'
-
-class MyBot < SlackRubyBot::Bot
-  command /.*\bhello\b.*$/i do |client, data, _match|
-    client.say(channel: data.channel, text: "hi")
-  end
-end
