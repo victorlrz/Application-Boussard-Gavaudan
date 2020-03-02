05f7f7d9682368b799d81cd1271ebf6e54da88df
Francois Baud <fxbaud@gmail.com>
2020-03-02 09:36:10 +0000

    bugs fixing on post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,13 +20,17 @@ class PostsController < ApplicationController
     if @post.save!
       redirect_to deals_path
     else
-      ender :new
+      render :new
+    end
   end
 
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def destroy
