bc5d8382d90b780ff138a182f4d24ace5b9e004c
Francois Baud <fxbaud@gmail.com>
2020-03-27 12:10:50 +0100

    with document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,3 @@
 class Document < ApplicationRecord
+  has_one_attached :document_pdf
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -6,6 +6,7 @@ class Target < ApplicationRecord
   has_many :rounds, :through => :deals
   validates :name, presence: true, uniqueness: true
   validates :sector, presence: true
+  after_commit :async_update
 
   algoliasearch do
   end
@@ -16,5 +17,9 @@ class Target < ApplicationRecord
     index = Algolia::Index.new('Target')
     index.add_object(self)
     puts "Reindex done"
+  end
+
+  def async_update
+    MarketdataJob.perform_later(self.identifier)
   end
 end
