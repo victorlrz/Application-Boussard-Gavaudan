d4dc536d6812168c1465395ac2635ce150dd01b9
Francois Baud <fxbaud@gmail.com>
2020-04-03 15:05:50 +0200

    with new stock front end

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,17 +9,37 @@ class StocksController < ApplicationController
   end
 
   def create
+    @stock = Stock.new(stock_params)
+    if @stock.save
+      redirect_to stocks_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @stock = Stock.find(params[:id])
   end
 
   def new
+    @stock = Stock.new
   end
 
   def update
+    @stock = Stock.find(params[:id])
+    if @stock.update(stock_params)
+      redirect_to stocks_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:name, :sector, :currency, :identifier)
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,10 @@
 class Stock < ApplicationRecord
   has_many :comments
   validates :name, presence: true, uniqueness: true
+  before_save :uppercase_name
+
+  private
+  def uppercase_name
+    name.upcase!
+  end
 end
