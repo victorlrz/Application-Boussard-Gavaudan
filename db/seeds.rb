f7607308ce55474d6907dbaff69ea607beee1a2f
Francois Baud <fxbaud@gmail.com>
2020-04-10 15:41:29 +0200

    with changes in new stocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,146 +1,222 @@
-stock_1= Stock.new(name: 'DJ EURO STOXX 50 P INDEX', identifier: 'SX5T:STX')
-stock_2= Stock.new(name: 'TELEPERFORMANCE', identifier: 'TEP:PAR')
-stock_3= Stock.new(name: 'ILIAD SA', identifier: 'ILD:PAR')
-stock_4= Stock.new(name: 'RENAULT SA', identifier: 'RNO:PAR')
-stock_5= Stock.new(name: 'ELIS FP', identifier: 'ELIS:PAR')
-stock_6= Stock.new(name: 'FAURECIA', identifier: 'EO:PAR')
-stock_7= Stock.new(name: 'LAGARDERE S.C.A.', identifier: 'MMB:PAR')
-stock_8= Stock.new(name: 'VICAT SA', identifier: 'VCT:PAR')
-stock_9= Stock.new(name: 'MERSEN', identifier: 'MRN:PAR')
-stock_10= Stock.new(name: 'REXEL SA', identifier: 'RXL:PAR')
-stock_11= Stock.new(name: 'VERALLIA', identifier: 'VRLA:PAR')
-stock_12= Stock.new(name: 'CELLNEX', identifier: 'CLNX:MCE')
-stock_13= Stock.new(name: 'EVOLUTION GAMING GROUP', identifier: 'EVO:STO')
-stock_14= Stock.new(name: 'ASM INTERNATIONAL NV', identifier: 'ASM:AEX')
-stock_15= Stock.new(name: 'SIG COMBIBLOC GRP N', identifier: 'SIGN:SWX')
-stock_16= Stock.new(name: 'POLYMETAL PLC', identifier: 'POLY:LSE')
-stock_17= Stock.new(name: 'PSP SWISS PROPERTY AG-REG', identifier: 'PSPN:SWX')
-stock_18= Stock.new(name: 'GALENICA SANTE', identifier: 'GALE:SWX')
-stock_19= Stock.new(name: 'HOLMEN AB-B SHARES', identifier: 'HOLM B:STO')
-stock_20= Stock.new(name: 'BALDER', identifier: 'BALD B:STO')
-stock_21= Stock.new(name: 'SCHNEIDER ELECTRIC SA', identifier: 'SU:PAR')
-stock_22= Stock.new(name: 'ASML HOLDING NV', identifier: 'ASML:AEX')
-stock_23= Stock.new(name: 'SARTORIUS PREF', identifier: 'SRTX:GER')
-stock_24= Stock.new(name: 'ELISA OYJ', identifier: 'ELISA:HEX')
-stock_25= Stock.new(name: 'NEXI SPA', identifier: 'NEXI:MIL')
-stock_26= Stock.new(name: 'FLUTTER ENTERTAINMENT', identifier: 'FLTR:ISE')
-stock_27= Stock.new(name: 'INGENICO', identifier: 'ING:PAR')
-stock_28= Stock.new(name: 'PENNON GROUP PLC', identifier: 'PNN:LSE')
-stock_29= Stock.new(name: 'BECHTLE AG', identifier: 'BC8X:GER')
-stock_30= Stock.new(name: 'DELIVERY HERO AG', identifier: 'DHERX:GER')
-stock_31= Stock.new(name: 'HANNOVER RUECKVERSICHERU-SE', identifier: 'HNRX.N:GER')
-stock_32= Stock.new(name: 'GALAPAGOS', identifier: 'GLPG:AEX')
-stock_33= Stock.new(name: 'GENMAB A/S', identifier: 'GMAB:CPH')
-stock_35= Stock.new(name: 'SCOUT24 AG', identifier: 'G24X.N:GER')
-stock_37= Stock.new(name: 'LONDON STOCK EXCHANGE GROUP', identifier: 'LSE:LSE')
-stock_38= Stock.new(name: 'TELE2 AB-B SHS', identifier: 'TEL2 B:STO')
-stock_39= Stock.new(name: 'BE SEMICONDUCTOR INDUSTRIES', identifier: 'BESI:AEX')
-stock_40= Stock.new(name: 'ENEL SPA', identifier: 'ENEL:MIL')
-stock_41= Stock.new(name: 'GJENSIDIGE FORSIKRING ASA', identifier: 'GJF:OSL')
-stock_42= Stock.new(name: 'LVMH MOET HENNESSY LOUIS VUI', identifier: 'LVMH:MIL')
-stock_43= Stock.new(name: 'HOWDEN JOINERY GROUP PLC', identifier: 'HWDN:LSE')
-stock_44= Stock.new(name: 'EDP-ENERGIAS DE PORTUGAL SA', identifier: 'EDP:LIS')
-stock_45= Stock.new(name: 'ALTICE NV - A', identifier: 'ATC:AEX')
-stock_46= Stock.new(name: 'AVEVA GROUP PLC', identifier: 'AVV:LSE')
-stock_47= Stock.new(name: 'LEGRAND SA', identifier: 'LR:PAR')
-stock_48= Stock.new(name: 'SWISS PRIME', identifier: 'SPSN:SWX')
-stock_49= Stock.new(name: 'DSV A/S', identifier: 'DSV:CPH')
-stock_50= Stock.new(name: 'SEGRO PLC', identifier: 'SGRO:LSE')
-stock_51= Stock.new(name: 'ORSTED A/S', identifier: 'ORSTED:CPH')
-stock_52= Stock.new(name: 'IDORSIA LTD', identifier: 'IDIA:SWX')
-stock_53= Stock.new(name: 'CASTELLUM AB', identifier: 'CAST:STO')
-stock_54= Stock.new(name: 'UNITE GROUP PLC', identifier: 'UTG:LSE')
-stock_55= Stock.new(name: 'AMPLIFON SPA', identifier: 'AMP:MIL')
-stock_56= Stock.new(name: 'FERRARI', identifier: 'RACE:MIL')
-stock_57= Stock.new(name: 'ICADE', identifier: 'ICAD:PAR')
-stock_58= Stock.new(name: 'PARTNERS GROUP HOLDING AG', identifier: 'PGHN:VTX')
-stock_59= Stock.new(name: 'NIBE INDUSTRIER AB B SH', identifier: 'NIBE%20B:STO')
-stock_60= Stock.new(name: 'STMICROELECTRONICS ITALY', identifier: 'STM:MIL')
-stock_61= Stock.new(name: 'HERA SPA', identifier: 'HER:MIL')
-stock_62= Stock.new(name: 'BAKKAFROST', identifier: 'BAKKA:OSL')
-stock_63= Stock.new(name: 'PERSIMMON PLC', identifier: 'PSN:LSE')
-stock_64= Stock.new(name: 'SAMHALL NORD-B RG', identifier: 'SBB%20B:STO')
-stock_65= Stock.new(name: 'NEMETSCHEK', identifier: 'NEMX:GER')
-stock_66= Stock.new(name: 'FERROVIAL SA', identifier: 'FER:MCE')
-stock_67= Stock.new(name: 'ZURICH INSURANCE GROUP AG', identifier: 'ZURN:VTX')
-stock_68= Stock.new(name: 'INMOBILIARIA COLONIAL SA', identifier: 'COL:MCE')
-stock_69= Stock.new(name: 'SOPRA GROUP', identifier: 'SOP:PAR')
-stock_70= Stock.new(name: 'NEXANS SA', identifier: 'NEX:PAR')
-stock_71= Stock.new(name: 'VEOLIA ENVIRONNEMENT', identifier: 'VIE:PAR')
-stock_72= Stock.new(name: 'INTERMEDIATE CAPITAL GROUP', identifier: 'ICP:LSE')
-stock_73= Stock.new(name: 'JM AB', identifier: 'JM:STO')
-stock_74= Stock.new(name: 'VISTRY GROUP', identifier: 'VTY:LSE')
-stock_75= Stock.new(name: 'REDROW PLC', identifier: 'RDW:LSE')
-stock_1.save
-stock_2.save
-stock_3.save
-stock_4.save
-stock_5.save
-stock_6.save
-stock_7.save
-stock_8.save
-stock_9.save
-stock_10.save
-stock_11.save
-stock_12.save
-stock_13.save
-stock_14.save
-stock_15.save
-stock_16.save
-stock_17.save
-stock_18.save
-stock_19.save
-stock_20.save
-stock_21.save
-stock_22.save
-stock_23.save
-stock_24.save
-stock_25.save
-stock_26.save
-stock_27.save
-stock_28.save
-stock_29.save
-stock_30.save
-stock_31.save
-stock_32.save
-stock_33.save
-stock_35.save
-stock_37.save
-stock_38.save
-stock_39.save
-stock_40.save
-stock_41.save
-stock_42.save
-stock_43.save
-stock_44.save
-stock_45.save
-stock_46.save
-stock_47.save
-stock_48.save
-stock_49.save
-stock_50.save
-stock_51.save
-stock_52.save
-stock_53.save
-stock_54.save
-stock_55.save
-stock_56.save
-stock_57.save
-stock_58.save
-stock_59.save
-stock_60.save
-stock_61.save
-stock_62.save
-stock_63.save
-stock_64.save
-stock_65.save
-stock_66.save
-stock_67.save
-stock_68.save
-stock_69.save
-stock_70.save
-stock_71.save
-stock_72.save
-stock_73.save
-stock_74.save
-stock_75.save
+stock_2= Stock.find(name: 'TELEPERFORMANCE'.id)
+stock_3= Stock.find(name: 'ILIAD SA'.id)
+stock_4= Stock.find(name: 'RENAULT SA'.id)
+stock_5= Stock.find(name: 'ELIS FP'.id)
+stock_6= Stock.find(name: 'FAURECIA'.id)
+stock_7= Stock.find(name: 'LAGARDERE S.C.A.'.id)
+stock_8= Stock.find(name: 'VICAT SA'.id)
+stock_9= Stock.find(name: 'MERSEN'.id)
+stock_10= Stock.find(name: 'REXEL SA'.id)
+stock_11= Stock.find(name: 'VERALLIA'.id)
+stock_12= Stock.find(name: 'CELLNEX'.id)
+stock_13= Stock.find(name: 'EVOLUTION GAMING GROUP'.id)
+stock_14= Stock.find(name: 'ASM INTERNATIONAL NV'.id)
+stock_15= Stock.find(name: 'SIG COMBIBLOC GRP N'.id)
+stock_16= Stock.find(name: 'POLYMETAL PLC'.id)
+stock_17= Stock.find(name: 'PSP SWISS PROPERTY AG-REG'.id)
+stock_18= Stock.find(name: 'GALENICA SANTE'.id)
+stock_19= Stock.find(name: 'HOLMEN AB-B SHARES'.id)
+stock_20= Stock.find(name: 'BALDER'.id)
+stock_21= Stock.find(name: 'SCHNEIDER ELECTRIC SA'.id)
+stock_22= Stock.find(name: 'ASML HOLDING NV'.id)
+stock_23= Stock.find(name: 'SARTORIUS PREF'.id)
+stock_24= Stock.find(name: 'ELISA OYJ'.id)
+stock_25= Stock.find(name: 'NEXI SPA'.id)
+stock_26= Stock.find(name: 'FLUTTER ENTERTAINMENT'.id)
+stock_27= Stock.find(name: 'INGENICO'.id)
+stock_28= Stock.find(name: 'PENNON GROUP PLC'.id)
+stock_29= Stock.find(name: 'BECHTLE AG'.id)
+stock_30= Stock.find(name: 'DELIVERY HERO AG'.id)
+stock_31= Stock.find(name: 'HANNOVER RUECKVERSICHERU-SE'.id)
+stock_32= Stock.find(name: 'GALAPAGOS'.id)
+stock_33= Stock.find(name: 'GENMAB A/S'.id)
+stock_34= Stock.find(name: 'LUNDBERGS AB-B SHS'.id)
+stock_35= Stock.find(name: 'SCOUT24 AG'.id)
+stock_36= Stock.find(name: 'ALSTOM'.id)
+stock_37= Stock.find(name: 'LONDON STOCK EXCHANGE GROUP'.id)
+stock_38= Stock.find(name: 'TELE2 AB-B SHS'.id)
+stock_39= Stock.find(name: 'BE SEMICONDUCTOR INDUSTRIES'.id)
+stock_40= Stock.find(name: 'ENEL SPA'.id)
+stock_41= Stock.find(name: 'GJENSIDIGE FORSIKRING ASA'.id)
+stock_42= Stock.find(name: 'LVMH MOET HENNESSY LOUIS VUI'.id)
+stock_43= Stock.find(name: 'HOWDEN JOINERY GROUP PLC'.id)
+stock_44= Stock.find(name: 'EDP-ENERGIAS DE PORTUGAL SA'.id)
+stock_45= Stock.find(name: 'ALTICE NV - A'.id)
+stock_46= Stock.find(name: 'AVEVA GROUP PLC'.id)
+stock_47= Stock.find(name: 'LEGRAND SA'.id)
+stock_48= Stock.find(name: 'SWISS PRIME'.id)
+stock_49= Stock.find(name: 'DSV A/S'.id)
+stock_50= Stock.find(name: 'SEGRO PLC'.id)
+stock_51= Stock.find(name: 'ORSTED A/S'.id)
+stock_52= Stock.find(name: 'IDORSIA LTD'.id)
+stock_53= Stock.find(name: 'CASTELLUM AB'.id)
+stock_54= Stock.find(name: 'UNITE GROUP PLC'.id)
+stock_55= Stock.find(name: 'AMPLIFON SPA'.id)
+stock_56= Stock.find(name: 'FERRARI'.id)
+stock_57= Stock.find(name: 'ICADE'.id)
+stock_58= Stock.find(name: 'PARTNERS GROUP HOLDING AG'.id)
+stock_59= Stock.find(name: 'NIBE INDUSTRIER AB B SH'.id)
+stock_60= Stock.find(name: 'STMICROELECTRONICS ITALY'.id)
+stock_61= Stock.find(name: 'HERA SPA'.id)
+stock_62= Stock.find(name: 'BAKKAFROST'.id)
+stock_63= Stock.find(name: 'PERSIMMON PLC'.id)
+stock_64= Stock.find(name: 'SAMHALL NORD-B RG'.id)
+stock_65= Stock.find(name: 'NEMETSCHEK'.id)
+stock_66= Stock.find(name: 'FERROVIAL SA'.id)
+stock_67= Stock.find(name: 'ZURICH INSURANCE GROUP AG'.id)
+stock_68= Stock.find(name: 'INMOBILIARIA COLONIAL SA'.id)
+stock_69= Stock.find(name: 'SOPRA GROUP'.id)
+stock_70= Stock.find(name: 'NEXANS SA'.id)
+stock_71= Stock.find(name: 'VEOLIA ENVIRONNEMENT'.id)
+stock_72= Stock.find(name: 'INTERMEDIATE CAPITAL GROUP'.id)
+stock_73= Stock.find(name: 'JM AB'.id)
+stock_74= Stock.find(name: 'VISTRY GROUP'.id)
+stock_75= Stock.find(name: 'REDROW PLC'.id)
+stock_bis_2=Portfolio.new(stock_id:stock_2, date: '07/04/2020', fund: 'BGEQT', weight:0.04, aum:19744454.08)
+stock_bis_3=Portfolio.new(stock_id:stock_3, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_4=Portfolio.new(stock_id:stock_4, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_5=Portfolio.new(stock_id:stock_5, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_6=Portfolio.new(stock_id:stock_6, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_7=Portfolio.new(stock_id:stock_7, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_8=Portfolio.new(stock_id:stock_8, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_9=Portfolio.new(stock_id:stock_9, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_10=Portfolio.new(stock_id:stock_10, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_11=Portfolio.new(stock_id:stock_11, date: '07/04/2020', fund: 'BGEQT', weight:0.03, aum:19744454.08)
+stock_bis_12=Portfolio.new(stock_id:stock_12, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_13=Portfolio.new(stock_id:stock_13, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_14=Portfolio.new(stock_id:stock_14, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_15=Portfolio.new(stock_id:stock_15, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_16=Portfolio.new(stock_id:stock_16, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_17=Portfolio.new(stock_id:stock_17, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_18=Portfolio.new(stock_id:stock_18, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_19=Portfolio.new(stock_id:stock_19, date: '07/04/2020', fund: 'BGEQT', weight:0.02, aum:19744454.08)
+stock_bis_20=Portfolio.new(stock_id:stock_20, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_21=Portfolio.new(stock_id:stock_21, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_22=Portfolio.new(stock_id:stock_22, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_23=Portfolio.new(stock_id:stock_23, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_24=Portfolio.new(stock_id:stock_24, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_25=Portfolio.new(stock_id:stock_25, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_26=Portfolio.new(stock_id:stock_26, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_27=Portfolio.new(stock_id:stock_27, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_28=Portfolio.new(stock_id:stock_28, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_29=Portfolio.new(stock_id:stock_29, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_30=Portfolio.new(stock_id:stock_30, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_31=Portfolio.new(stock_id:stock_31, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_32=Portfolio.new(stock_id:stock_32, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_33=Portfolio.new(stock_id:stock_33, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_34=Portfolio.new(stock_id:stock_34, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_35=Portfolio.new(stock_id:stock_35, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_36=Portfolio.new(stock_id:stock_36, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_37=Portfolio.new(stock_id:stock_37, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_38=Portfolio.new(stock_id:stock_38, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_39=Portfolio.new(stock_id:stock_39, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_40=Portfolio.new(stock_id:stock_40, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_41=Portfolio.new(stock_id:stock_41, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_42=Portfolio.new(stock_id:stock_42, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_43=Portfolio.new(stock_id:stock_43, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_44=Portfolio.new(stock_id:stock_44, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_45=Portfolio.new(stock_id:stock_45, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_46=Portfolio.new(stock_id:stock_46, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_47=Portfolio.new(stock_id:stock_47, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_48=Portfolio.new(stock_id:stock_48, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_49=Portfolio.new(stock_id:stock_49, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_50=Portfolio.new(stock_id:stock_50, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_51=Portfolio.new(stock_id:stock_51, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_52=Portfolio.new(stock_id:stock_52, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_53=Portfolio.new(stock_id:stock_53, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_54=Portfolio.new(stock_id:stock_54, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_55=Portfolio.new(stock_id:stock_55, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_56=Portfolio.new(stock_id:stock_56, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_57=Portfolio.new(stock_id:stock_57, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_58=Portfolio.new(stock_id:stock_58, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_59=Portfolio.new(stock_id:stock_59, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_60=Portfolio.new(stock_id:stock_60, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_61=Portfolio.new(stock_id:stock_61, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_62=Portfolio.new(stock_id:stock_62, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_63=Portfolio.new(stock_id:stock_63, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_64=Portfolio.new(stock_id:stock_64, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_65=Portfolio.new(stock_id:stock_65, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_66=Portfolio.new(stock_id:stock_66, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_67=Portfolio.new(stock_id:stock_67, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_68=Portfolio.new(stock_id:stock_68, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_69=Portfolio.new(stock_id:stock_69, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_70=Portfolio.new(stock_id:stock_70, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_71=Portfolio.new(stock_id:stock_71, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_72=Portfolio.new(stock_id:stock_72, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_73=Portfolio.new(stock_id:stock_73, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_74=Portfolio.new(stock_id:stock_74, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_75=Portfolio.new(stock_id:stock_75, date: '07/04/2020', fund: 'BGEQT', weight:0.01, aum:19744454.08)
+stock_bis_2.save
+stock_bis_3.save
+stock_bis_4.save
+stock_bis_5.save
+stock_bis_6.save
+stock_bis_7.save
+stock_bis_8.save
+stock_bis_9.save
+stock_bis_10.save
+stock_bis_11.save
+stock_bis_12.save
+stock_bis_13.save
+stock_bis_14.save
+stock_bis_15.save
+stock_bis_16.save
+stock_bis_17.save
+stock_bis_18.save
+stock_bis_19.save
+stock_bis_20.save
+stock_bis_21.save
+stock_bis_22.save
+stock_bis_23.save
+stock_bis_24.save
+stock_bis_25.save
+stock_bis_26.save
+stock_bis_27.save
+stock_bis_28.save
+stock_bis_29.save
+stock_bis_30.save
+stock_bis_31.save
+stock_bis_32.save
+stock_bis_33.save
+stock_bis_34.save
+stock_bis_35.save
+stock_bis_36.save
+stock_bis_37.save
+stock_bis_38.save
+stock_bis_39.save
+stock_bis_40.save
+stock_bis_41.save
+stock_bis_42.save
+stock_bis_43.save
+stock_bis_44.save
+stock_bis_45.save
+stock_bis_46.save
+stock_bis_47.save
+stock_bis_48.save
+stock_bis_49.save
+stock_bis_50.save
+stock_bis_51.save
+stock_bis_52.save
+stock_bis_53.save
+stock_bis_54.save
+stock_bis_55.save
+stock_bis_56.save
+stock_bis_57.save
+stock_bis_58.save
+stock_bis_59.save
+stock_bis_60.save
+stock_bis_61.save
+stock_bis_62.save
+stock_bis_63.save
+stock_bis_64.save
+stock_bis_65.save
+stock_bis_66.save
+stock_bis_67.save
+stock_bis_68.save
+stock_bis_69.save
+stock_bis_70.save
+stock_bis_71.save
+stock_bis_72.save
+stock_bis_73.save
+stock_bis_74.save
+stock_bis_75.save
