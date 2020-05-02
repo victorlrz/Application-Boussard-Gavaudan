f76acebfdc6ecf837cd1fbf4c616a6882c230173
Francois Baud <fxbaud@gmail.com>
2020-05-02 10:33:43 +0200

    log in sign up and sign in forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :phone_number, :location, :first_name, :last_name, :tax_residency, :tax_number, :source_of_wealth])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :company, :first_name, :last_name])
   end
 end
