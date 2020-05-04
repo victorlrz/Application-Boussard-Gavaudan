062baa5690ff607d6422acb9e3ee4b6b6c070145
Victor <larrezetvictor@gmail.com>
2020-05-04 18:56:53 +0200

    :zap: feature v1.1.0

--- app/javascript/features/apiftimes.js
+++ app/javascript/features/apiftimes.js
@@ -1,15 +1,10 @@
-// -----------------------------------------------FINANCIAL-TIMES-API-JS-------------------------
-// branch name : feature
-// Feature created in order to recover data from the Financial Times
+require("dotenv").config();
+const secretKey = process.env.FT_KEY;
 
-// Curations allow the API consumer to specify a curated set of content and describes the scope against which a Query will operate.
-// AVAILABLE QUERY CONTEXT CURATIONS : "ARTICLES, BLOGS, PAGES, PODCASTS, VIDEOS"
+const titleContainerElement = document.querySelector(".deal_newsflow");
 
-// Aspects allow consumers to specify which elements of content they wish to receive within the results. Aspects can be provided in the resultContext
-// AVAILABLE RESULTS CONTEXT : ASPECTS : “lifecycle”,“location”,“master”,“summary”,“title”
-
-// <%= @deal.acquirer.name %>
-
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// Params à modifier
 const searchParam = {
   queryString: "banks",
   queryContext: {
@@ -18,13 +13,36 @@ const searchParam = {
   resultContext: {
     aspects: ["title", "lifecycle"],
     maxResults: 20,
-    offset: 21,
-    sortOrder: "ASC",
-    sortField: "title",
-    facets: { names: ["people"], maxElements: 20, minThreshold: 1 },
+    // offset: 21,
+    // sortOrder: "ASC",
+    // sortField: "title",
+    // facets: { names: ["people"], maxElements: 20, minThreshold: 1 },
   },
 };
-const searchContentAPI = async () => {
+
+const headlines = [];
+
+const createTitleElement = (title) => {
+  const div = document.createElement("div");
+  div.innerHTML = `<p>${title.text}</p>`;
+  return div;
+};
+
+const displayHeadlines = () => {
+  const titleNode = headlines.map((title) => {
+    return createTitleElement(title);
+  });
+  titleContainerElement.innerHTML = "";
+  titleContainerElement.append(...titleNode);
+};
+
+const addHeadline = (text) => {
+  headlines.push({
+    text,
+  });
+};
+
+const searchHeadlines = async () => {
   const url = "https://api.ft.com/content/search/v1";
   const json = JSON.stringify(searchParam);
   try {
@@ -34,41 +52,20 @@ const searchContentAPI = async () => {
       body: json,
       headers: {
         "Content-Type": "application/json",
-        "X-Api-Key": "59cbaf20e3e06d3565778e7b3d64dd02f06e492d9f7e1adb8c648332",
+        "X-Api-Key": secretKey,
       },
     });
     if (response.ok) {
       const dataAPI = await response.json();
       for (let i = 0; i < searchParam.resultContext.maxResults; i++) {
-        let articleId = dataAPI.results[0].results[0].id;
-        // console.log(articleId); //DEBUG
-        getContentId(articleId);
+        addHeadline(dataAPI.results[0].results[i].title.title);
       }
+      displayHeadlines();
+      // console.log(headline);
     }
   } catch (e) {
     console.error("e : ", e);
   }
 };
 
-searchContentAPI();
-// WARNING: Proxy URL
-
-const getContentId = async (id) => {
-  const url = `https://api.ft.com/content/{${id}}`;
-  // console.log(url); //DEBUG
-  try {
-    const response = await fetch(proxyurl + url, {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Api-Key": "59cbaf20e3e06d3565778e7b3d64dd02f06e492d9f7e1adb8c648332",
-      },
-    });
-    if (response.ok) {
-      const dataAPI = await response.json();
-      // console.log("NO BUG") //DEBUG
-      console.log(dataAPI); // We just print the object in the console ATM.
-    }
-  } catch (e) {
-    console.error("e : ", e);
-  }
-};
+searchHeadlines();
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,19 @@
 import "bootstrap";
 import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
 import flatpickr from "flatpickr";
-import Typed from 'typed.js';
-import algoliasearch from 'algoliasearch';
+import Typed from "typed.js";
+import algoliasearch from "algoliasearch";
 import trix from "trix";
 import "trix/dist/trix.css";
-import '../styles/application';
+import "../styles/application";
 
-window.jQuery = $;
-window.$ = $;
+import "../features/apiftimes.js";
+
+// window.jQuery = $;
+// window.$ = $;
 
 // import 'datatables.net-dt';
 // import 'datatables.net-bs4/js/dataTables.bootstrap4.min';
-
-console.log("Hello");
 
 flatpickr("#deal_date");
 flatpickr("#ipo_date");
@@ -21,37 +21,8 @@ flatpickr("#post_date");
 flatpickr("#comment_date");
 flatpickr("#document_date");
 
-
 // $(document).ready( function () {
 //   $("#datatables-reponsive").DataTable({
 //     responsive: true
 //   });
 // });
-
-
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "https://api.ft.com/content/search/v1";
-const searchApi = async () => {
-  const searchParam = {
-    queryString: "bayer",
-  };
-  const json = JSON.stringify(searchParam);
-  try {
-    const response = await fetch(proxyurl + url, {
-      method: "POST",
-      body: json,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-        "X-Api-Key": "59cbaf20e3e06d3565778e7b3d64dd02f06e492d9f7e1adb8c648332",
-      },
-    });
-    if (response.ok) {
-      const dataAPI = await response.json();
-      console.log(dataAPI.results);
-    }
-  } catch (e) {
-    console.error("e : ", e);
-  }
-};
-searchApi();
