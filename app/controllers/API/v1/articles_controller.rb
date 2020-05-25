ceb74c8021afe6c3d89ad593ea39559b30cfd8db
Victor <larrezetvictor@gmail.com>
2020-05-25 10:50:03 +0200

    :zap: Securing API

--- app/controllers/API/v1/articles_controller.rb
+++ app/controllers/API/v1/articles_controller.rb
@@ -1,33 +1,38 @@
-module Api
-    module V1
-        class ArticlesController < ApplicationController
-            skip_before_action :verify_authenticity_token
+class Api::V1::ArticlesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :validate_api_key!
+    
+    def index
+        articles = Comment.order('id DESC, created_at DESC');
+        render json: {status: 'SUCCESS', message:'Loaded articles', data:articles},status: :ok
+    end
 
-            def index
-                articles = Comment.order('created_at DESC');
-                render json: {status: 'SUCCESS', message:'Loaded articles', data:articles},status: :ok
-            end
+    def show
+        article = Comment.where(stock_id: Comment.find(params[:id])).order('id DESC, created_at DESC')
+        render json: {status: 'SUCCESS', message:'Loaded article', data:article},status: :ok
+    end
 
-            def show
-                article = Comment.where(stock_id: Comment.find(params[:id]))
-                render json: {status: 'SUCCESS', message:'Loaded article', data:article},status: :ok
-            end
-
-            def create
-                article = Comment.new(article_params)
-
-                if article.save
-                    render json: {status: 'SUCCESS', message:'Saved article', data:article},status: :ok
-                else
-                    render json: {status: 'ERROR', message:'Article not saved', data:article.errors},status: :unprocessable_entity
-                end
-            end
-
-            private 
-
-            def article_params
-                params.permit(:stock, :article, :date, :category, :content, :source, :stock_id, :title, :cloudinary, :comment_link)
-            end
+    def create
+        article = Comment.new(article_params)
+        if article.save
+            render json: {status: 'SUCCESS', message:'Saved article', data:article},status: :ok
+        else
+            render json: {status: 'ERROR', message:'Article not saved', data:article.errors},status: :unprocessable_entity
         end
     end
-end
+
+    private
+    def article_params
+        params.require(:article).permit(:id, :date, :category, :content, :source, :stock_id, :title, :cloudinary, :comment_link)
+    end
+
+    def has_valid_api_key?
+        request.headers['X-Api-key'] == ENV['API_KEY']
+    end
+
+    def validate_api_key!
+        return head :forbidden unless has_valid_api_key?
+    end
+ end
+
+ 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    validates :id, presence: true
     validates :date, presence: true
     validates :stock_id, presence: true
     validates :title, presence: true
