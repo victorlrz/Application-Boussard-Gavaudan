a343fe2df1f729b4917bb17ec2c5ebcf92089e47
Francois Baud <fxbaud@gmail.com>
2020-05-26 16:27:07 +0200

    with front end on ipo index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,17 +17,17 @@ import 'jquery';
 import 'datatables.net';
 
 
-$(document).ready( function () {
-  $('#my_data').DataTable();
-} );``
-
 // Add some code with jQuery:
 $(document).ready(function() {
   console.log('jQuery just checked that the DOM is ready!');
 });
 
 
-import "../styles/application";
-
 import "../features/apiftimes.js";
 import "../features/apimorningstar.js";
+
+import "../styles/application";
+
+$(document).ready( function () {
+  $('#my_data').DataTable();
+} );
