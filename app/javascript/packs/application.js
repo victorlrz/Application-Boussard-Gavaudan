fc09d11f6b0eb83b8ab04668e2c8d175466de674
Victor <larrezetvictor@gmail.com>
2020-06-15 13:38:37 +0100

    :zap: portfolio front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,9 +25,10 @@ import "../features/apiftimes.js";
 // import "../features/apimorningstar.js";
 
 import "../styles/application";
+import "../frontend/menu/test";
 
 //Front end
-import "../frontend/navbar";
+import "../frontend/header/navbar";
 $(document).ready(function () {
   $("#my_data").DataTable();
 });
