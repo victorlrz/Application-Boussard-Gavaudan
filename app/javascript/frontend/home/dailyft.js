4340080ae73e8404c68307f7d3984721d100a8c8
Victor <larrezetvictor@gmail.com>
2020-07-17 13:17:48 +0100

    :zap: FT daily

--- app/javascript/frontend/home/dailyft.js
+++ app/javascript/frontend/home/dailyft.js
@@ -4,8 +4,8 @@ const dailyFtTitle = document.querySelector(".name-daily-ft");
 const rubyDatas = dailyFt.dataset.daily;
 const dailyDatas = rubyDatas.split(/"[\]]|[\[]"|", "/);
 
-const tabAfterSplice = [];
-const dailyObjectFt = [];
+let tabAfterSplice = [];
+let dailyObjectFt = [];
 
 //@function pour ajouter les éléments au daily Object
 const addDailyObj = (title, url, compagny) => {
@@ -26,21 +26,25 @@ const spliceEmptyIndex = (tab, tabToAdd) => {
   return tabToAdd;
 };
 
-spliceEmptyIndex(dailyDatas, tabAfterSplice);
-
-const createDailyFtObj = (sortTab) => {
+//@Fonction pour créer l'objet contenant tous les titres de la journée.
+const createDailyFtObj = (tab) => {
   let compt = 0;
   for (let i = 0; i < tab.length / 3; i++) {
-    addDailyObj(sortTab[compt], sortTab[compt + 1], sortTab[compt + 2]);
+    addDailyObj(tab[compt], tab[compt + 1], tab[compt + 2]);
     compt += 3;
-    console.log(compt);
   }
 };
 
-createDailyFtObj(tabAfterSplice);
-
-const changeTitle = () => {
-  dailyFt.innerHTML = dailyDatas[0];
+//@Fonction pour assigner le titre de l'index i
+//@Fonction pour Assigner un autre index en fonction du clic
+//@Fonction pour assigner un autre index en fonction du temps
+//Attention aux conditions si vide.
+const main = () => {
+  tabAfterSplice = [];
+  dailyObjectFt = [];
+  spliceEmptyIndex(dailyDatas, tabAfterSplice);
+  createDailyFtObj(tabAfterSplice);
+  console.log(dailyObjectFt); //A ce niveau JSON des articles du jour
 };
 
-changeTitle();
+main();
