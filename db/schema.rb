3da4115d269e9ed84f610abdd0525077a6e234f5
Francois Baud <fxbaud@gmail.com>
2020-07-22 09:22:08 +0100

    Victor's button

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_07_21_110420) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "acquirers", force: :cascade do |t|
