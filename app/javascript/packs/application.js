496b5d7b8542dcede567d53988bf53347e986cb2
Francois Baud <fxbaud@gmail.com>
2020-07-23 19:57:21 +0100

    work in progress for datatables

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -42,7 +42,6 @@ import "../frontend/stock/show/value_animation";
 import "../frontend/home/test";
 import "../frontend/home/dailyft";
 
-const element = document.getElementById('stocksbyfix');
 
 
 
